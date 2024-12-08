2e4ba6dfe686197718d1bc6bc288b593a6e137cc
winncoder <thangtnsa000@gmail.com>
2024-12-09 02:14:44 +0700

    fix(artall): update baseUrl

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import './Main.css';
 
 const queryClient = new QueryClient();
 
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.baseURL =
+	'https://57d8-2402-800-6294-490a-60b6-14fb-fa90-9eaa.ngrok-free.app/api/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
