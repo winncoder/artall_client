b54da400ced2344d295729f056146db14c371408
winncoder <thangtnsa000@gmail.com>
2024-12-09 17:26:37 +0700

    fix(artall): update buildspec

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './Main.css';
 const queryClient = new QueryClient();
 
 axios.defaults.baseURL =
-	'https://c641-2402-800-6294-490a-45c8-668a-71e1-9113.ngrok-free.app/api/';
+	'https://64f4-2402-800-6294-490a-45c8-668a-71e1-9113.ngrok-free.app/api/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
